5630ae6a97d9960fd02b7fbd02d09c8033949fdc
MerLin8756 <ilagosteev@gmail.com>
2026-08-17 13:21:47 +0300

    Хэширование пароля

--- logical/hash_password.py
+++ logical/hash_password.py
@@ -1,1 +1,13 @@
-"""Файл для хэширования пароля будем импортироваться в файле с работой юзера"""
+"""Файл для хэширования пароля будем подтаскивать данные из сайта, драть пароль если пользователь новый и
+хэщировать добавляя его в базу"""
+import hashlib
+
+
+def hash_password(psswd:str )->str:
+    crypt =hashlib.sha256()
+    b =bytes(psswd,'utf-8')
+    crypt.update(b)
+    return crypt.hexdigest()
+print(hash_password(''))
+
+
